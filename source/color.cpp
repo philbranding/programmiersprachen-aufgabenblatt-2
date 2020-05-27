234581843c760d000007d067614031521b97e6d9
philbranding <philbranding@gmail.com>
2020-05-27 10:40:09 +0200

    Aufgabe 2.7 color.cpp

---
+++ source/color.cpp
@@ -0,0 +1,15 @@
+#include "color.hpp"
+#include "window.hpp"
+
+
+Color::Color(): red{0}, green{0}, blue{0}{
+
+}
+
+Color::Color(float r): red{r}, green{r}, blue{r}{
+
+}
+
+Color::Color(float red, float green, float blue){
+
+}
